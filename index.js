4ece0074c0203d09c75c49d7f17ac4437487d8e0
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-07-08 17:19:03 +0300

    refactor routes

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-require('dotenv').config()
-const cors = require('cors')
-const express = require('express')
-const mongoose = require('mongoose')
-const mongoString = process.env.DATABASE_URL
-const routes = require('./routes/routes')
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const routes = require('./routes/routes');
 
-mongoose.connect(
-  process.env.DATABASE_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    console.log('connected')
-  }
-)
+mongoose
+  .connect(mongoString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.log('Error connecting to the database:', error);
+  });
 
-const database = mongoose.connection
-const app = express()
+const database = mongoose.connection;
+const app = express();
 
 database.on('error', (error) => {
-  console.log(error)
-})
+  console.log(error);
+});
 
 database.once('connected', () => {
-  console.log('Database Connected')
-})
-app.use(cors())
-app.use(express.json())
-app.use('/', routes)
+  console.log('Database Connected');
+});
+app.use(cors());
+app.use(express.json());
+app.use('/', routes);
 
 app.listen(process.env.PORT || 8000, (err) => {
-  if (err) throw err
-  console.log('Server started')
-})
+  if (err) throw err;
+  console.log('Server started');
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,43 +1,43 @@
-const express = require('express')
-const Model = require('../model/model')
-const router = express.Router()
+const express = require('express');
+const Model = require('../model/model');
+const router = express.Router();
 
-var fs = require('fs')
-var path = require('path')
-var multer = require('multer')
-var cloudinary = require('cloudinary').v2
-require('dotenv/config')
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+require('dotenv/config');
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
-})
+});
 
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads')
+    cb(null, 'uploads');
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now())
+    cb(null, file.fieldname + '-' + Date.now());
   },
-})
-var upload = multer({ storage: storage })
+});
+const upload = multer({ storage: storage });
 
 router.get('/', (req, res) => {
   Model.find({}, (err, items) => {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
-      res.json({ items: items })
+      res.json({ items: items });
     }
-  })
-})
+  });
+});
 
 //Post Method
-// localhost:8000/api/post
-router.post('/post', upload.single('image'), async (req, res) => {
+// localhost:8000/
+router.post('/', upload.single('image'), async (req, res) => {
   try {
-    const result = await cloudinary.uploader.upload(req.file.path)
+    const result = await cloudinary.uploader.upload(req.file.path);
     const data = new Model({
       title: req.body.title,
       briefDesc: req.body.briefDesc,
@@ -46,64 +46,60 @@ router.post('/post', upload.single('image'), async (req, res) => {
       demoLink: req.body.demoLink,
       tech: req.body.tech,
       image: result.secure_url,
-    })
-    await data.save()
-    res.json(data)
+    });
+    await data.save();
+    res.json(data);
   } catch (error) {}
-})
+});
 
 //Get all Method
-// localhost:8000/api/getAll
-
-router.get('/getAll', async (req, res) => {
+// localhost:8000/
+router.get('/', async (req, res) => {
   try {
-    const data = await Model.find()
-    res.json(data)
+    const data = await Model.find();
+    res.json(data);
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
 //Get by ID Method
-// localhost:8000/api/getOne/:id
-
-router.get('/getOne/:id', async (req, res) => {
+// localhost:8000/:id
+router.get('/:id', async (req, res) => {
   try {
-    const data = await Model.findById(req.params.id)
-    res.json(data)
+    const data = await Model.findById(req.params.id);
+    res.json(data);
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
 //Update by ID Method
-// localhost:8000/api/update/:id
-
-router.patch('/update/:id', async (req, res) => {
+// localhost:8000/:id
+router.patch('/:id', async (req, res) => {
   try {
-    const id = req.params.id
-    const updatedData = req.body
-    const options = { new: true }
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
 
-    const result = await Model.findByIdAndUpdate(id, updatedData, options)
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
 
-    res.send(result)
+    res.send(result);
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    res.status(400).json({ message: error.message });
   }
-})
+});
 
 //Delete by ID Method
-// localhost:8000/api/delete/:id
-
-router.delete('/delete/:id', async (req, res) => {
+// localhost:8000/:id
+router.delete('/:id', async (req, res) => {
   try {
-    const id = req.params.id
-    const data = await Model.findByIdAndDelete(id)
-    res.send(`Document with ${data.title} has been deleted..`)
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.title} has been deleted..`);
   } catch (error) {
-    res.status(400).json({ message: error.message })
+    res.status(400).json({ message: error.message });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
